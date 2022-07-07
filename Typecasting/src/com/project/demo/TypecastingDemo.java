aef012c82c0b7a3f9ece98c58394bf60bf4af628
user <user@DESKTOP-7FNJ8MC>
2022-07-07 12:46:02 +0530

    PUSH

---
+++ Typecasting/src/com/project/demo/TypecastingDemo.java
@@ -0,0 +1,12 @@
+package com.project.demo;
+
+public class TypecastingDemo {
+	public static void main(String[] args) {
+//		byte i=(byte)135;
+		int i=30;
+		byte j=(byte) i;
+		System.out.println(i);
+		System.out.println(j);
+	}
+
+}
